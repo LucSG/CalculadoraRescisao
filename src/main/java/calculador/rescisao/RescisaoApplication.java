5079d8e6ad8e7346c7af55ade29186dc03ee1aa3
Lucas Santos Guimarães <50470894+LucSG@users.noreply.github.com>
2025-03-15 13:04:28 -0500

    Inicio do projeto Spring

---
+++ src/main/java/calculador/rescisao/RescisaoApplication.java
@@ -0,0 +1,13 @@
+package calculador.rescisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescisaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calculador/rescisao/RescisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package calculador.rescisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
