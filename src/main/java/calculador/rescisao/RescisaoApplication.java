87f565e4414b7176625c9b4e349495cb5c0e88d2
Lucas Santos Guimarães <50470894+LucSG@users.noreply.github.com>
2025-04-08 12:11:24 -0500

    Update RescisaoApplication.java

--- src/main/java/calculador/rescisao/RescisaoApplication.java
+++ src/main/java/calculador/rescisao/RescisaoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RescisaoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RescisaoApplication.class, args);
 	}
