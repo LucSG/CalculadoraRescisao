5dc1eec73ad61bf8dc5a1fcb204d2f54e28f15a4
Lucas Santos Guimarães <50470894+LucSG@users.noreply.github.com>
2025-04-08 22:58:29 -0500

    Update RescisaoController.java

--- src/main/java/calculador/rescisao/controller/RescisaoController.java
+++ src/main/java/calculador/rescisao/controller/RescisaoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rescisao")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://calculadorarescisao.site")
 public class RescisaoController {
 
     @Autowired
